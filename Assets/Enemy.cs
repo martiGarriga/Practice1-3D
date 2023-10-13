6d56539bbef395457c0f14ccbb0c2b4d1e7606da
martiGarriga <mgarrigal@edu.tecnocampus.cat>
2023-10-13 17:08:09 +0200

    Half Enemy

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Agent;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
@@ -23,7 +23,9 @@ public class Enemy : MonoBehaviour
     public float m_MaxDistanceToHear;
     public float m_MaxDistanceToSee;
     public LayerMask m_SeesPlayerLayerMask;
-    
+    float m_VisionConeAngle;
+
+
     void Awake()
     {
         m_NavMeshAgent = GetComponent<NavMeshAgent>();
@@ -39,73 +41,97 @@ public class Enemy : MonoBehaviour
     {
         switch(m_State)
         {
-            
+            case TState.IDLE:
+                UpdateIdleState();
+                    break;
+            case TState.PATROL:
+                UpdatePatrolState();
+                break;
+            case TState.CHASE:
+                UpdateChaseState();
+                break;
+            case TState.ATTACK:
+                UpdateAttackState();
+                break;
+            case TState.HIT:
+                UpdateHitState();
+                break;
+            case TState.ALERT:
+                UpdateAlertState();
+                break;
+            case TState.DIE:
+                UpdateDieState();
+                break;
+
+
         }
         
     }
     void SetIdleState()
     {
-        m_State = IDLE;
+        m_State = TState.IDLE;
     }
     void SetPatrolState()
     {
-        m_State = PATROL;
+        m_State = TState.PATROL;
     }
     void SetChaseState()
     {
-        m_State = CHASE;
+        m_State = TState.CHASE;
     }
     void SetDieState()
     {
-        m_State = DIE;
+        m_State = TState.DIE;
     }
     void SetHitState()
     {
-        m_State =HIT;
+        m_State = TState.HIT;
     }
     void SetAlertState()
     {
-        m_State = ALERT;
+        m_State = TState.ALERT;
     }
     void SetAttackState()
     {
-        m_State = ATTACK;
+        m_State = TState.ATTACK;
     }
+
     void UpdateIdleState()
     {
         SetPatrolState();
     }
     void UpdatePatrolState()
     {
-        if(m_NavMeshAgent.hasPath && m_NavMeshAgent.pathStatus = NavMeshPathStatus.PathComplete)
+        SetPatrolState();
+        if(m_NavMeshAgent.hasPath && m_NavMeshAgent.pathStatus == NavMeshPathStatus.PathComplete)
         {
             MoveNextPatrol();
         }
     }
     void UpdateChaseState()
     {
-
+        SetChaseState();
     }
     void UpdateDieState()
     {
-
+        SetDieState();
     }
     void UpdateHitState()
     {
-
+        SetHitState();
     }
     void UpdateAlertState()
     {
-
+        SetAlertState();
     }
     void UpdateAttackState()
     {
-        
+        SetAttackState();
     }
     void SetNextChasePosition()
     {
-        Vector3 l_PlayerPosition = GameController.GetGameController()m_Player.transform.position;
-        Vector3 l_EnemyPosition;
+        Vector3 l_PlayerPosition = GameController.GetGameController().m_Player.transform.position;
+        Vector3 l_EnemyPosition = transform.position;
 
         Vector3 l_Direction = (l_EnemyPosition -l_PlayerPosition);
         l_Direction.Normalize();
@@ -128,19 +154,19 @@ public class Enemy : MonoBehaviour
     }
     bool HearPlayer()
     {
-        Vector3 l_PlayerPosition = GameController.GetGameController()m_Player.transform.position;
-        Vector3 l_EnemyPosition;
+        Vector3 l_PlayerPosition = GameController.GetGameController().m_Player.transform.position;
+        Vector3 l_EnemyPosition = transform.position;
         float l_Distance = Vector3.Distance(l_PlayerPosition,l_EnemyPosition);
         return l_Distance < m_MaxDistanceToHear;
     }
     bool SeesPlayer()
     {
-        Vector3 l_PlayerPosition = GameController.GetGameController()m_Player.transform.position;
-        Vector3 l_EnemyPosition;
+        Vector3 l_PlayerPosition = GameController.GetGameController().m_Player.transform.position;
+        Vector3 l_EnemyPosition = transform.position;
         float l_Distance = Vector3.Distance(l_PlayerPosition,l_EnemyPosition);
         if(l_Distance<m_MaxDistanceToSee)
         {
-            Vector3 l_EnemyForward = transform.forwrard;
+            Vector3 l_EnemyForward = transform.forward;
             l_EnemyForward.y = 0.0f;
             l_EnemyForward.Normalize();
             Vector3 l_VectorEnemyToPlayer = l_PlayerPosition -l_EnemyPosition;
@@ -150,7 +176,7 @@ public class Enemy : MonoBehaviour
             if(l_DotAngle > Mathf.Cos(Mathf.Deg2Rad*m_VisionConeAngle/2.0f))
             {
                 Ray l_Ray = new Ray(l_EnemyPosition+Vector3.up*1.8f, l_VectorEnemyToPlayer);
-                if(!Physics.RayCast(l_Ray, l_Distance, m_SeesPlayerLayerMask.value))
+                if(!Physics.Raycast(l_Ray, l_Distance, m_SeesPlayerLayerMask.value))
                 return true;
             }
         }
