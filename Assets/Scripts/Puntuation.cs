6fd33f232bfff0b5fcfcd65814c7f7b36ca4a43a
martiGarriga <mgarrigal@edu.tecnocampus.cat>
2023-10-17 19:17:56 +0200

    Terrain / Maquina estados en proceso

--- Assets/Scripts/Puntuation.cs
+++ Assets/Scripts/Puntuation.cs
@@ -1,11 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Puntuation : MonoBehaviour
 {
-    public int PlusPoints(int points)
+    private int m_points = 0;
+
+    public Text m_PointsText;
+    private void OnEnable()
     {
-        return points + 100;
+        ShootingGalery.OnTimeOut += RestartPoints;
+    }
+    private void OnDisable()
+    {
+        ShootingGalery.OnTimeOut -= RestartPoints;
+
+    }
+    private void Update()
+    {
+        m_PointsText.text = m_points.ToString();
+    }
+    public void PlusPoints()
+    {
+         m_points=m_points + 100;
+    }
+    public void RestartPoints()
+    {
+        m_points = m_points = 0;
     }
 }
